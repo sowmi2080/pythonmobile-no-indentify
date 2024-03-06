22b3a30dc16d5344581babf2a9c02ff12c886b11
sowmi2080 <125257423+sowmi2080@users.noreply.github.com>
2024-03-06 19:11:32 +0530

    Add files via upload

---
+++ Location_Tracker1-main/Location_Tracker1-main/main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+
+if __name__ == '__main__':
+    print_hi('Pyf5Charm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
